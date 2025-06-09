598cde96395b6604d7776a3cbc2f828b9b329497
Ritwik Saha <riksaha1998@gmail.com>
2025-06-09 21:12:57 +0530

    New repo created

--- src/interfaceExmple/Interface.java
+++ src/interfaceExmple/Interface.java
@@ -1,13 +1,15 @@
 package interfaceExmple;
 
+//This annotation ensures that This is the "Functional Interface"
+@FunctionalInterface
 public interface Interface {
 	
 	
-
+	//In functionalInterface any number of Static and Default method 
 	static void staticMethod() {
 		System.out.println("In Static methos of the Interface" );
 	}
-	
+	//Only one Abstract method. SAM = Single Abstract Method
 	public void show();
 	
 	default void defaultMethod() {
